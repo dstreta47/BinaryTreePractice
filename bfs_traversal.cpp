ac8df1768cf4a6d10724b3f8d4d0ce3cca188ce0
Amit Rana <43043638+dstreta47@users.noreply.github.com>
2021-08-20 15:38:53 -0500

    id 7734566

---
+++ bfs_traversal.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;}};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+    if(d==-1){
+        return NULL;
+    }
+    node* root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void print(node *root){
+    if(root==NULL){
+        return;
+    }
+    print(root->left);
+    print(root->right);
+    cout<<root->data<<" ";
+}
+
+int height(node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int ls = height(root->left);
+    int rs = height(root->right);
+    return max(ls,rs)+1;
+}
+
+void bfs(node *root){
+    queue<node *> q;
+    q.push(root);
+    while(!q.empty()){
+        node *f =q.front();
+        cout<< f->data<<",";
+        q.pop();
+        if(f->left){
+            q.push(f->left);
+        }
+        if(f->right){
+            q.push(f->right);
+        }
+    }
+    return;
+}
+
+int main() {
+    node* root =buildTree();
+   // print(root);
+   // cout<<height(root)<<endl;;
+    bfs(root);
+
+    
+    return 0;
+}
